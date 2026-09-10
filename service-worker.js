3695ffc28d1d9f029a56d6e31ba120f72e29be8a
Maradan Tom <maradantom@gmail.com>
2026-09-10 23:27:38 +0200

    auto: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@
 // (un seul SW peut être enregistré au scope /, donc on fusionne les deux)
 try { importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js'); } catch (_) {}
 
-const CACHE = 'bde-cread-v7';
+const CACHE = 'bde-cread-v8';
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE)
-      .then(c => c.addAll(['/assets/Logo.png', '/assets/icon-192.png']))
+      .then(c => c.addAll(['/assets/Logo.png', '/assets/icon-192.png?v=3']))
       .then(() => self.skipWaiting())
   );
 });
